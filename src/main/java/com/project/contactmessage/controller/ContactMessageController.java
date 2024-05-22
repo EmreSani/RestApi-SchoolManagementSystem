afc946cf1f2e366ced3398a81e39b3ee4ddc94ca
EmreSani <emresani1998@gmail.com>
2024-05-22 14:44:19 +0300

    contact message methods

--- src/main/java/com/project/contactmessage/controller/ContactMessageController.java
+++ src/main/java/com/project/contactmessage/controller/ContactMessageController.java
@@ -2,16 +2,19 @@ package com.project.contactmessage.controller;
 
 import com.project.contactmessage.dto.ContactMessageRequest;
 import com.project.contactmessage.dto.ContactMessageResponse;
+import com.project.contactmessage.entity.ContactMessage;
 import com.project.contactmessage.service.ContactMessageService;
 import com.project.payload.response.business.ResponseMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/contactMessages")
@@ -21,12 +24,84 @@ public class ContactMessageController {
     private final ContactMessageService contactMessageService;
 
     @PostMapping("/save") //http://localhost:8080/contactMessages/save + JSON + POST
-    public ResponseMessage<ContactMessageResponse> saveContact(@Valid @RequestBody ContactMessageRequest contactMessageRequest){
+    public ResponseMessage<ContactMessageResponse> saveContact(@Valid @RequestBody ContactMessageRequest contactMessageRequest) {
 
         return contactMessageService.save(contactMessageRequest);
 
     }
 
+    // Not: ******************************************** getAllByPage ***************************************
+    @GetMapping("/page")
+    public Page<ContactMessageResponse> getAllByPage(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sort") String prop,
+            @RequestParam("direction") Sort.Direction direction
+    ) {
+        return contactMessageService.getAllByPage(page, size, prop, direction);
+    }
 
+    // Not: ************************************* searchByEmailByPage ***************************************
+    @GetMapping("/searchByEmail")
+    public Page<ContactMessageResponse> searchByEmail(
+            @RequestParam("email") String email,
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sort") String prop,
+            @RequestParam("direction") Sort.Direction direction
+    ) {
+        return contactMessageService.searchByEmail(email, page, size, prop, direction);
+    }
+
+    // Not: *************************************** searchBySubjectByPage ***************************************
+    @GetMapping("/searchBySubject")
+    public Page<ContactMessageResponse> searchBySubject(
+            @RequestParam("subject") String subject,
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sort") String prop,
+            @RequestParam("direction") Sort.Direction direction
+    ) {
+        return contactMessageService.searchBySubject(subject, page, size, prop, direction);
+    }
+    // Not: searchByDateBetween ***************************************
+
+    // Not: searchByTimeBetween ***************************************
+
+    // Not: *********************************** deleteByIdParam ***************************************
+    @DeleteMapping("/query")
+    public ResponseEntity<Map<String, String>> deleteContactMessageByIdParam(@RequestParam("id") Long id) {
+
+        contactMessageService.deleteById(id);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("message", "contactmessage is deleted successfully");
+        map.put("status", "true");
+        return new ResponseEntity<>(map, HttpStatus.OK);
+
+    }
+
+    // Not: ***************************************** deleteById ***************************************
+    @DeleteMapping("/{id}")
+    public ResponseMessage<ContactMessageResponse> deleteContactMessageById(@PathVariable("id") Long id) {
+        return contactMessageService.deleteByIdPath(id);
+
+    }
+
+    // Not: *********************************** getByIdWithParam ***************************************
+    @GetMapping("/query")
+    public ResponseMessage<ContactMessageResponse> getContactMessageWithParam(@RequestParam("id") Long id) {
+
+        return contactMessageService.ContactMessageToResponse(id);
+
+    }
+
+    // Not: ************************************ getByIdWithPath ***************************************
+    @GetMapping("/{id}")
+    public ResponseMessage<ContactMessageResponse> getContactMessageWithPathVariable(@PathVariable("id") Long id) {
+
+        return contactMessageService.ContactMessageToResponse(id);
+
+    }
 
 }
--- src/main/java/com/project/contactmessage/service/ContactMessageService.java
+++ src/main/java/com/project/contactmessage/service/ContactMessageService.java
@@ -5,8 +5,13 @@ import com.project.contactmessage.dto.ContactMessageResponse;
 import com.project.contactmessage.entity.ContactMessage;
 import com.project.contactmessage.mapper.ContactMessageMapper;
 import com.project.contactmessage.repository.ContactMessageRepository;
+import com.project.exception.ResourceNotFoundException;
 import com.project.payload.response.business.ResponseMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -29,18 +34,71 @@ public class ContactMessageService {
     }
 
     // Not: ******************************************** getAllByPage ***************************************
+    public Page<ContactMessageResponse> getAllByPage(int page, int size, String prop, Sort.Direction direction) {
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, prop));
+
+       return contactMessageRepository.findAll(pageable).map(createContactMessage::contactMessageToResponse);
+
+    }
 
     // Not: ************************************* searchByEmailByPage ***************************************
+    public Page<ContactMessageResponse> searchByEmail(String email, int page, int size, String prop, Sort.Direction direction) {
+    Pageable pageable = PageRequest.of(page,size, Sort.by(direction, prop));
+
+    return contactMessageRepository.findByEmailEquals(email, pageable).map(createContactMessage::contactMessageToResponse);
+    }
 
     // Not: *************************************** searchBySubjectByPage ***************************************
+    public Page<ContactMessageResponse> searchBySubject(String subject, int page, int size, String prop, Sort.Direction direction) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, prop));
+
+        return contactMessageRepository.findBySubjectEquals(subject, pageable).map(createContactMessage::contactMessageToResponse);
+    }
+
+
 
     // Not: searchByDateBetween ***************************************
 
     // Not: searchByTimeBetween ***************************************
 
+    //findContactById methodu oluşturup bu methodu silme işlemleri için kullanabiliriz
+    public ContactMessage findContactMessage(Long id){
+
+        return contactMessageRepository.findById(id).orElseThrow(()->
+                new ResourceNotFoundException("Contact Message not found with id: "+ id));
+
+    }
+
+    public ResponseMessage<ContactMessageResponse> ContactMessageToResponse(Long id){
+
+     ContactMessageResponse contactMessageResponse = createContactMessage.contactMessageToResponse(findContactMessage(id));
+
+     return ResponseMessage.<ContactMessageResponse>builder()
+             .message("Contact message found successfully")
+             .httpStatus(HttpStatus.OK)
+             .object(contactMessageResponse).build();
+
+    }
+
     // Not: *********************************** deleteByIdParam ***************************************
+    public void deleteById(Long id) {
+        ContactMessage contactMessage = findContactMessage(id);
+        contactMessageRepository.deleteById(id); //böylece silmeye çalıştığımız contact message objesinin null olup olmaması kontrolleri sağlanmış oldu.
+    }
+
+    public ResponseMessage<ContactMessageResponse> deleteByIdPath(Long id) {
+        ContactMessage contactMessage = findContactMessage(id);
+        contactMessageRepository.deleteById(id);
+
+        return ResponseMessage.<ContactMessageResponse>builder()
+                .message("Contact message deleted succesfully")
+                .httpStatus(HttpStatus.OK)
+                .build();
+    }
 
     // Not: ***************************************** deleteById ***************************************
+
 
     // Not: *********************************** getByIdWithParam ***************************************
 
