bb6524711468d0d345d75a962e9da29d8cc6560e
EmreSani <emresani1998@gmail.com>
2024-05-29 23:02:24 +0300

    about user

--- src/main/java/com/project/payload/messages/ErrorMessages.java
+++ src/main/java/com/project/payload/messages/ErrorMessages.java
@@ -7,4 +7,10 @@ public class ErrorMessages {
 
     public static final String NOT_PERMITTED_METHOD_MESSAGE = "You do not have any permission to do this operation";
     public static final String PASSWORD_NOT_MATCHED = "Your passwords are not matched" ;
+    public static final String ALREADY_REGISTER_MESSAGE_USERNAME = "Error: User with username %s already registered" ;
+    public static final String ALREADY_REGISTER_MESSAGE_SSN = "Error: User with ssn %s already registered" ;
+    public static final String ALREADY_REGISTER_MESSAGE_EMAIL = "Error: User with email %s already registered" ;
+    public static final String ALREADY_REGISTER_MESSAGE_PHONE = "Error: User with phone number %s already registered" ;
+
+    public static final String ROLE_NOT_FOUND = "There is no role like that , check the database" ;
 }
--- src/main/java/com/project/repository/user/UserRepository.java
+++ src/main/java/com/project/repository/user/UserRepository.java
@@ -6,5 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsernameEquals(String username);
+
     User findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    boolean existsBySsn(String ssn);
+
+    boolean existsByPhoneNumber(String phone);
+
+    boolean existsByEmail(String email);
+
 }
